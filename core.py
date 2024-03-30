7d1c2ee69ed3690f83bc80cbcbffe5d25f87ae34
Victor <victor.alicino@gmail.com>
2024-03-30 18:24:42 -0300

    .

--- core.py
+++ core.py
@@ -1,4 +1,75 @@
-import sys
+#!/usr/bin/python3
 
-if __name__ == "__main__":
-    sys.exit(0)
+""" The main file of the program. """
+
+import sys
+import os
+
+from finals import REQUIRED_PYTHON_VER, SUPPORTED_OS
+
+
+def check_configurations() -> None:
+    """Check if the configurations are valid."""
+    print("Checking configurations...")
+    config_folder = "./configurations"
+
+    # Check if the configurations folder exists
+    if not os.path.exists(config_folder) and not os.path.isdir(config_folder):
+        print("ERROR: Configurations folder does not exist.")
+        exit(1)
+    print(f"Configurations Folder found at {config_folder}")
+
+    # Check the YAML files
+    print("Checking YAML files...")
+    for file in os.listdir(config_folder):
+        if file.endswith(".yaml"):
+            print(f"Found: {file}")
+
+
+def check_os() -> None:
+    """Check if the right OS is running."""
+    print("Checking OS...")
+    print(f"OS: {sys.platform}")
+
+    is_supported: bool = False
+    for os_name in SUPPORTED_OS:
+        if sys.platform == os_name:
+            is_supported = True
+            break
+    if is_supported is False:
+        print(f"{sys.platform} is currentely not supported.")
+        sys.exit(1)
+
+
+def check_python() -> None:
+    """Check if the right Python version is running."""
+    print("Checking Python version...")
+    print(f"Python version: {sys.version_info[0]}.{sys.version_info[1]}.{sys.version_info[2]}")
+    if sys.version_info < REQUIRED_PYTHON_VER:
+        print(f"ERROR: Python {REQUIRED_PYTHON_VER[0]}."
+              "{REQUIRED_PYTHON_VER[1]}."
+              "{REQUIRED_PYTHON_VER[2]} "
+              "for higher is required.")
+        sys.exit(1)
+
+
+def main() -> int:
+    """The main function."""
+    exit_code: int = 0
+
+    check_python()
+    check_os()
+    check_configurations()
+    # TODO: Check files integrity
+
+    # TODO: Load plugins
+    # TODO: Connect to database
+    # TODO: Start entity manager
+    # TODO: Start event manager
+    # TODO: Start task manager
+
+    return exit_code
+
+
+if __name__ == '__main__':
+    exit(main())
---
+++ finals.py
@@ -0,0 +1,9 @@
+"""Constants for the project."""
+
+from typing import Final
+
+# Python Version
+REQUIRED_PYTHON_VER: Final = (3, 10, 0)
+
+# OS Supported
+SUPPORTED_OS: Final = ["linux", "darwin", "win32"]
