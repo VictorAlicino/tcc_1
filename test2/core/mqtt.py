617de01f50257b68a58f8e77b75832c457c22808
Victor <victor.alicino@gmail.com>
2024-04-17 08:01:00 -0300

    .

--- test2/core/mqtt.py
+++ test2/core/mqtt.py
@@ -1,33 +1,23 @@
 """Communication handler"""
 
-from dataclasses import dataclass
-from typing import Literal as typing_literal
-from paho.mqtt.properties import Properties as paho_mqtt_properties
-from paho.mqtt.client import Client as paho_mqtt_client
-
-@dataclass
-class MQTTConfig:
-    """MQTT communication specific configurations data type"""
-    # Note that this does follow the required arguments by
-    # PAHO implementation.
-    keep_alive: int | None
-    bind_address: str | None
-    bind_port: str | None
-    clean_start: bool | typing_literal[3] | None
-    properties: paho_mqtt_properties | None
-type mqtt_config_t = MQTTConfig
+from paho.mqtt import client
+from paho.mqtt.enums import CallbackAPIVersion
 
 
 class MQTT:
     """Communication handler"""
 
-    def __init__(self):
-        self.broker_host: str = None
-        self.broker_port: str = None
-        self.secure_connection: bool = True
-        self.mqtt_config: mqtt_config_t = None
+    def __init__(self, client_name: str):
+        # MQTT Client Configuration (see PAHO MQTT Docs)
+        self.client = client.Client(
+            callback_api_version= CallbackAPIVersion.VERSION2,
+            client_id=client_name,
+            clean_session=True,
+            userdata=None,
+            protocol=client.MQTTv5,
+            transport='tcp',
+            reconnect_on_failure=True,
+            manual_ack=False
+        )
 
-        self.client: paho_mqtt_client = None
-
-    def connect(self) -> None:
-        ...
+    def config(self, )
--- test2/main.py
+++ test2/main.py
@@ -1,11 +1,77 @@
 """The main entry point for the application."""
 
 import sys
+import os
+from typing import Final
 
-def main():
-    """The main entry point for the application."""
-    print("Hello, World!")
-    return 0
+# Python Version
+REQUIRED_PYTHON_VER: Final = (3, 12, 0)
+
+# OS Supported
+SUPPORTED_OS: Final = ["linux", "darwin", "win32"]
+
+
+def check_configurations() -> None:
+    """Check if the configurations are valid."""
+    print("Checking configurations...")
+    config_folder = "./configurations"
+
+    # Check if the configurations folder exists
+    if not os.path.exists(config_folder) and not os.path.isdir(config_folder):
+        print("ERROR: Configurations folder does not exist.")
+        sys.exit(1)
+    print(f"Configurations Folder found at {config_folder}")
+
+    # Check the YAML files
+    print("Checking YAML files...")
+    for file in os.listdir(config_folder):
+        if file.endswith(".yaml"):
+            print(f"Found: {file}")
+
+
+def check_os() -> None:
+    """Check if the right OS is running."""
+    print("Checking OS...")
+    print(f"OS: {sys.platform}")
+
+    is_supported: bool = False
+    for os_name in SUPPORTED_OS:
+        if sys.platform == os_name:
+            is_supported = True
+            break
+    if is_supported is False:
+        print(f"{sys.platform} is currentely not supported.")
+        sys.exit(1)
+
+
+def check_python() -> None:
+    """Check if the right Python version is running."""
+    print("Checking Python version...")
+    print(f"Python version: {sys.version_info[0]}.{sys.version_info[1]}.{sys.version_info[2]}")
+    if sys.version_info < REQUIRED_PYTHON_VER:
+        print(f"ERROR: Python {REQUIRED_PYTHON_VER[0]}."
+              f"{REQUIRED_PYTHON_VER[1]}."
+              f"{REQUIRED_PYTHON_VER[2]} "
+              f"or higher is required.")
+        sys.exit(1)
+
+
+def main() -> int:
+    """The main function."""
+    exit_code: int = 0
+
+    check_python()
+    check_os()
+    #check_configurations()
+    # TODO: Check files integrity
+
+    # TODO: Load drivers
+    # TODO: Connect to database
+    # TODO: Start entity manager
+    # TODO: Start event manager
+    # TODO: Start task manager
+
+    return exit_code
 
 if __name__ == "__main__":
     sys.exit(main())
