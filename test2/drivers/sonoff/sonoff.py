270987ab97bf2fc858a8547703031da52f3566f1
Victor Alicino <victor.alicino@gmail.com>
2024-04-19 17:17:18 -0300

    .

--- test2/drivers/sonoff/sonoff.py
+++ test2/drivers/sonoff/sonoff.py
@@ -1,10 +1,11 @@
 """SONOFF Connection Driver"""
 
 from enum import Enum
+from ipaddress import ip_address
 import requests
 
 class SONOFFDeviceType(Enum):
-    """Enumare for Sonoff Device Types avaiable for the DIY API"""
+    """Enumerate for Sonoff Device Types available for the DIY API"""
     DIY_PLUG = 0    # BASICR3 | RFR3 | MINI | MINIR2 | MINIR3
     DIYLIGHT = 1    # D1
     DIY_METER = 2   # SPM-Main
@@ -12,19 +13,12 @@ class SONOFFDeviceType(Enum):
 
 type sonoff_device_type_t = SONOFFDeviceType
 
-class SONOFFdriver:
+class SonoffDevice:
     """Driver to talk to Sonoff Devices with the DIY API enabled"""
-    def __init__(self):
-        self.ip_address: list[
-            bytes,
-            bytes,
-            bytes,
-            bytes
-            ]
+    def __init__(self, ip: ip_address):
+        self.ip_address: ip_address = ip
         self.hostname: str
         self.service_type: str
         self.device_type: sonoff_device_type_t
+        self.service_instace_name: str
 
-
-def discover_devices():
-    ...
