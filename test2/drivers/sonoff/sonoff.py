6e70b8930aaa9e99fd6d0ac7b5c3a3387d6bc2d4
Victor <victor.alicino@gmail.com>
2024-04-22 14:10:32 -0300

    .

--- test2/drivers/sonoff/sonoff.py
+++ test2/drivers/sonoff/sonoff.py
@@ -1,17 +1,21 @@
 """SONOFF Connection Driver"""
 
+import asyncio
 from enum import Enum
-import sys
 from ipaddress import ip_address
 # Non-Standard Libraries
 from zeroconf import ServiceBrowser, Zeroconf
 
 class SonoffDeviceType(Enum):
-    """Enumerate for Sonoff Device Types available for the DIY API"""
+    """Enumerate for Sonoff Device Types"""
     DIY_PLUG = 0    # BASICR3 | RFR3 | MINI | MINIR2 | MINIR3
     DIYLIGHT = 1    # D1
     DIY_METER = 2   # SPM-Main
     DIY_LIGHT = 3   # B02-BL-A60 | B05-BL-A19 | B05-BL-A60
+
+    # Not DIY mode (Encrypted)
+    STRIP = 4
+    PLUG = 5
 
 type sonoff_device_type_t = SonoffDeviceType
 
@@ -21,57 +25,57 @@ class SonoffDevice:
         self.ip_address: ip_address = ip
         self.hostname: str
         self.port: int = 8081
-        self.bssid: bytearray
-        self.device_type: sonoff_device_type_t
+        self.bssid: bytearray | None
+        self.device_type: sonoff_device_type_t | str
         self.service_instace_name: str
         self.device_id: str
 
     def __str__(self) -> str:
-        return (f"Sonoff Device {self.hostname} at {self.ip_address}"
-                f" with device_id {self.device_id}"
-                f" and type {self.device_type.name}"
-                f" on port {self.port}"
-                f" with BSSID {self.bssid.hex()}"
-                f" and service instance name {self.service_instace_name}")
+        return (f"Sonoff Device {self.hostname} at {self.ip_address}\n"
+                f" DeviceID              -> {self.device_id}\n"
+                f" Type                  -> {self.device_type}\n"
+                f" Port                  -> {self.port}\n"
+                f" BSSID                 -> {self.bssid}\n"
+                f" Service instance name -> {self.service_instace_name}\n")
 
 known_devices: list = []
 
-async def search_devices() -> list[SonoffDevice]:
-    """Search for Sonoff Devices in the network"""
+def _found_new_device(name, info) -> None:
+    ip = info.addresses[0]
+    new_device = SonoffDevice(ip_address(f'{ip[0]}.{ip[1]}.{ip[2]}.{ip[3]}'))
+    new_device.hostname = name.split('.')[0]
+    new_device.port = info.port
+    new_device.bssid = None
+    new_device.device_type = bytes(info.properties[('type').encode('utf-8')]).decode()
+    new_device.service_instace_name = info.type
+    new_device.device_id = bytes(info.properties[('id').encode('utf-8')]).decode()
+    #print(f"\n[mDNS] Found new {new_device.device_type} -> "
+    #      f"{new_device.hostname} @ {new_device.ip_address}"
+    #      f" | id: {new_device.device_id}")
+    #print(properties)
+    known_devices.append(new_device)
+
 
 class MDNSListener:
     """Listener for Sonoff mDNS Service"""
 
-    def remove_service(self, zeroconf, type, name):
+    def remove_service(self, zeroconf, type, name): # pylint: disable=redefined-builtin
         """"Remove Service"""
         # do nothing
 
-    def add_service(self, zeroconf, type, name):
+    def add_service(self, zeroconf, type, name): # pylint: disable=redefined-builtin
         """Add a Device to the known devices list"""
-        if type == "_ewelink._tcp.local.":
-            info = zeroconf.get_service_info(type, name)
-            if info:
-                ip = info.addresses[0]
-                new_device = SonoffDevice(ip_address(f'{ip[0]}.{ip[1]}.{ip[2]}.{ip[3]}'))
-                new_device.hostname = name
-                new_device.port = info.port
-                new_device.service_instace_name = info.type
-                print(f"Found device {new_device.hostname} at {new_device.ip_address}")
-                print(info)
-                known_devices.append(new_device)
+        info = zeroconf.get_service_info(type, name)
+        if info:
+            _found_new_device(name, info)
 
-zeroconf = Zeroconf()
-listener = MDNSListener()
-browser = ServiceBrowser(zeroconf, "_ewelink._tcp.local.", listener)
 
-try:
-    input("Press enter to stop discovering...\n")
-finally:
+async def search_devices(timer: int) -> list[SonoffDevice]:
+    """Search for Sonoff Devices in the network"""
+    zeroconf = Zeroconf()
+    listener = MDNSListener()
+    ServiceBrowser(zeroconf, "_ewelink._tcp.local.", listener)
+
+    await asyncio.sleep(timer)
     zeroconf.close()
-
-print("Discovered devices with ewelink service:")
-for device in known_devices:
-    print(device)
-    print()
-
-sys.exit(0)
+    return known_devices
--- test2/drivers/sonoff/sonoff_light.py
+++ test2/drivers/sonoff/sonoff_light.py
@@ -7,6 +7,7 @@ from ipaddress import ip_address
 # Non-Standard Libraries
 sys.path.append('../../') # pylint: disable=wrong-import-position
 import aiohttp
+import sonoff # pylint: disable=import-error
 from sonoff import SonoffDevice # pylint: disable=import-error
 from core.devices.light import OpusLight
 
@@ -20,47 +21,67 @@ class SonoffLight(OpusLight):
     async def on(self) -> None:
         async with aiohttp.ClientSession() as session:
             async with session.post(
-                    f'http://{self.sonoff_link.ip_address}:{self.sonoff_link.port}/zeroconf/switch',
+                    f'http://{self.sonoff_link.ip_address}:{self.sonoff_link.port}'
+                    f'/zeroconf/switch',
                     # "{"deviceid": "10016d3258", "data": { "switch": "on" }}"
                     data= json.dumps({
                         "deviceid": self.sonoff_link.device_id,
                         "data": { "switch": "on"}
                         })
                     ) as resp:
-                    print(resp)
+                print(resp)
 
     async def off(self) -> None:
         async with aiohttp.ClientSession() as session:
             async with session.post(
-                    f'http://{self.sonoff_link.ip_address}:{self.sonoff_link.port}/zeroconf/switch',
+                    f'http://{self.sonoff_link.ip_address}:{self.sonoff_link.port}'
+                    f'/zeroconf/switch',
                     # "{"deviceid": "10016d3258", "data": { "switch": "off" }}"
                     data= json.dumps({
                         "deviceid": self.sonoff_link.device_id,
                         "data": { "switch": "off"}
                         })
                     ) as resp:
-                    print(resp)
+                print(resp)
 
-async def debug() -> int:
+    async def get_wifi_strength(self) -> int:
+        """Get WiFi Strength from device"""
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                    f'http://{self.sonoff_link.ip_address}:{self.sonoff_link.port}'
+                    f'/zeroconf/signal_strength',
+                    # "{"deviceid": "10016d3258", "data": {}}"
+                    data= json.dumps({
+                        "deviceid": self.sonoff_link.device_id,
+                        "data": {}
+                        })
+                    ) as resp:
+                print(resp)
+
+async def debug() -> None:
     """Main for Debug porpuses"""
     luz1 = SonoffLight("ablublé")
     luz1.sonoff_link = SonoffDevice(ip_address("192.168.15.2"))
     luz1.sonoff_link.device_id = "10016d3258"
     luz1.sonoff_link.hostname = "eWeLink_10016d3258._ewelink._tcp.local."
 
-    #await luz1.on()
-    #await asyncio.sleep(2)
-    #await luz1.off()
-    #await asyncio.sleep(2)
-    return 0
+    devices = await sonoff.search_devices(5)
+    for device in devices:
+        print(device)
+    while True:
+        try:
+            await luz1.on()
+            await asyncio.sleep(2)
+            await luz1.off()
+            await asyncio.sleep(2)
+            await luz1.get_wifi_strength()
+        except Exception as e:
+            print(f"Error: {e}")
+            raise e
 
 if __name__ == "__main__":
     try:
-        while True:
-            try:
-                asyncio.run(debug())
-            except Exception as e:
-                print(f"Error: {e}")
-                raise e
+        asyncio.run(debug())
     except KeyboardInterrupt:
+        print("Interrupted by User, closing")
         sys.exit(127)
