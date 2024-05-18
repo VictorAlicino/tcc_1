9ee769aeb1c5dcf71760e866975e273966ba9a7b
Bruno Gaspar <bruninhoogaspar@gmail.com>
2024-05-18 03:15:19 -0300

    fix: change details and fix safe area insets

--- test3/conductor/src/app.tsx
+++ test3/conductor/src/app.tsx
@@ -1,31 +1,29 @@
+import { Roboto_400Regular, Roboto_500Medium, Roboto_700Bold, useFonts } from "@expo-google-fonts/roboto";
 import { registerRootComponent } from "expo";
 import { StatusBar } from "expo-status-bar";
-import { 
-  useFonts, 
-  Roboto_400Regular, 
-  Roboto_500Medium, 
-  Roboto_700Bold 
-} from "@expo-google-fonts/roboto";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import colors from "tailwindcss/colors";
 
+import { Loading } from "@/components/loading";
 import { Home } from "@/screens/home";
 
 function App() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
     Roboto_500Medium,
-    Roboto_700Bold
+    Roboto_700Bold,
   });
 
   if (!fontsLoaded) {
-    return null;
+    return <Loading />;
   }
 
   return (
-    <>
+    <GestureHandlerRootView style={{ flex: 1, backgroundColor: colors.zinc[900] }}>
       <Home />
       <StatusBar style="light" />
-    </>
-  )
+    </GestureHandlerRootView>
+  );
 }
 
 registerRootComponent(App);
--- test3/conductor/src/components/building-button.tsx
+++ test3/conductor/src/components/building-button.tsx
@@ -1,4 +1,5 @@
 import { TouchableOpacity, TouchableOpacityProps } from "react-native";
+import colors from "tailwindcss/colors";
 
 import { Icon } from "@/components/icon";
 import { Text } from "@/components/text";
@@ -10,7 +11,16 @@ interface BuildingButtonProps extends TouchableOpacityProps {
 
 export function BuildingButton({ title, open, ...props }: BuildingButtonProps) {
   return (
-    <TouchableOpacity className="flex-row space-x-2 items-center self-center px-4" activeOpacity={0.7} {...props}>
+    <TouchableOpacity
+      className="flex-row space-x-2 items-center justify-center px-6 py-2 rounded-lg"
+      style={{
+        borderWidth: 1,
+        borderStyle: "dashed",
+        borderColor: colors.zinc[800],
+      }}
+      activeOpacity={0.7}
+      {...props}
+    >
       <Text className="font-500 text-lg">{title}</Text>
       <Icon name={open ? "arrow-drop-up" : "arrow-drop-down"} size={32} />
     </TouchableOpacity>
