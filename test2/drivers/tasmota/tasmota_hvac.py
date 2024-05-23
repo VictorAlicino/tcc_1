e7d952b8501645c2fca4962b974a9fdab883df6c
Victor Alicino <victor.alicino@gmail.com>
2024-05-23 15:12:24 -0300

    .

--- test2/drivers/tasmota/tasmota_hvac.py
+++ test2/drivers/tasmota/tasmota_hvac.py
@@ -111,4 +111,5 @@ class TasmotaHVAC(OpusHVAC):
         log.debug("\t├── Building ID: %s", self.building_id)
         log.debug("\t├── Driver: %s", self.driver)
         log.debug("\t├── Vendor: %s", self.vendor)
+        log.debug("\t├── MQTT Name: %s", self.mqtt_name)
         log.debug("\t└── Type: %s", self.type)
